eb0d0f4421b88ee291a3541d5612d94b4a23ce75
Rogelio Ceballos Castillo <rogelioceballos218@gmail.com>
2025-10-15 17:13:56 -0600

    URL updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-  base: "https://rogelio-cc.github.io/web-portfolio"
+  base: "https://Rogelio-CC.github.io/web-portfolio"
 });
